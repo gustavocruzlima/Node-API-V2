019356ab1f2e7a553792aa049b8d74d04bc94b55
gustavocruzlima <gustavofcruzlima@gmail.com>
2020-11-18 21:21:20 -0300

    adicionando parametro para conexão ao banco com o mongoose

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 // Connect to Mongoose and set connection variable
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 
 // Added check for DB connection
